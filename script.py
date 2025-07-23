a3e81a99805d026dccfee1367e24126f13be8817
swapnilshrirao9 <swapnil.shrirao9@gmail.com>
2025-07-23 14:08:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ TOMCAT_XML_PATH = "/opt/tomcat/conf/tomcat-users.xml"
 # Jenkins Info
 JENKINS_URL = "http://182.12.0.12:8080"  # <<<< Jenkins server IP
 JENKINS_USER = "admin"                   # <<<< Jenkins admin user
-JENKINS_TOKEN = "your_jenkins_api_token" # <<<< Jenkins API token
+JENKINS_TOKEN = "admin" # <<<< Jenkins API token
 CREDENTIAL_ID = "tomcat-user-creds"      # <<<< Jenkins credential ID
 
 # SSH Key Path (default: ~/.ssh/id_rsa)
